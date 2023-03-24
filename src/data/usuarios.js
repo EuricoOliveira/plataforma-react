a176d3d1b2781cf7706de5573e4a00e66594c655
https://github.com/EuricoOliveira/html.git <https://github.com/EuricoOliveira/html.git>
2023-03-23 22:22:50 -0300

    plataforma-react

---
+++ src/data/usuarios.js
@@ -0,0 +1,30 @@
+export const usuarios = [
+    {
+        id: 1,
+        nome: "Eurico Oliveira",
+        idade: 28,
+        email: "juca@email.com",
+        senha: "123"
+    },
+    {
+        id: 2,
+        nome: "Julia Santiago",
+        idade: 23,
+        email: "julia@email.com",
+        senha: "321"
+    },
+    {
+        id: 3,
+        nome: "Manuela Santiago Oliveira",
+        idade: 1,
+        email: "manuela@email.com",
+        senha: "456"
+    },
+    {
+        id: 4,
+        nome: "Diane Santana",
+        idade: 52,
+        email: "diane@email.com",
+        senha: "789"
+    },
+];
